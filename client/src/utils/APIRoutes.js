a6353c9491cec1c4f2c8d790baa6f96ed1c1a87c
OmkarRevankar76 <94139056+OmkarRevankar76@users.noreply.github.com>
2022-04-06 12:44:45 +0530

    first commit

---
+++ client/src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "https://sonicaton.herokuapp.com/";
+export const loginRoute = `https://sonicaton.herokuapp.com/api/auth/login`;
+export const registerRoute = `https://sonicaton.herokuapp.com/api/auth/register`;
+export const logoutRoute = `https://sonicaton.herokuapp.com/api/auth/logout`;
+export const allUsersRoute = `https://sonicaton.herokuapp.com/api/auth/allusers`;
+export const sendMessageRoute = `https://sonicaton.herokuapp.com/api/messages/addmsg`;
+export const recieveMessageRoute = `https://sonicaton.herokuapp.com/api/messages/getmsg`;
+export const setAvatarRoute = `https://sonicaton.herokuapp.com/api/auth/setavatar`;
